33d7502f715b81e04b902d69035a795d6f272fb3
sarathsr785 <59958337+sarathsr785@users.noreply.github.com>
2020-02-04 18:32:38 +0530

    Create maintain.sh

---
+++ maintain.sh
@@ -0,0 +1,171 @@
+#!/bin/bash
+clear
+setenforce 0 >> /dev/null 2>&1
+
+#FILEREPO=http://files.virtualizor.com
+LOG=/root/maintain.log
+touch $LOG
+#----------------------------------
+# Detecting the Architecture
+#----------------------------------
+if ([ `uname -i` == x86_64 ] || [ `uname -m` == x86_64 ]); then
+        echo "ARCH=64"
+else
+        echo "ARCH=32"
+fi
+
+echo "-----------------------------------------------"
+echo " Welcome to  Installer"
+echo "-----------------------------------------------"
+echo " "
+if ! [ -f /usr/bin/yum ] ; then
+                echo "YUM wasnt found on the system. Please install YUM !"
+                echo "Exiting installer"
+                exit 1;
+        fi
+yum update
+
+echo "1)Changing SSH Port"
+
+
+if [ -e "/etc/ssh/sshd_config" ];then
+[ -z "`grep ^Port /etc/ssh/sshd_config`" ] && ssh_port=22 || ssh_port=`grep ^Port /etc/ssh/sshd_config | awk '{print $2}'`
+echo $ssh_port
+
+if [ $ssh_port == "22" ]; then
+
+
+#if   [ -f  $ssh_port=="22" ] ; then
+
+while :; do echo
+read -p "Please input SSH port(Default: $ssh_port): " SSH_PORT
+[ -z "$SSH_PORT" ] && SSH_PORT=$ssh_port
+if [ $SSH_PORT -eq 22 >/dev/null 2>&1 -o $SSH_PORT -gt 1024 >/dev/null 2>&1 -a $SSH_PORT -lt 65535 >/dev/null 2>&1 ];then
+break
+else
+echo "${CWARNING}input error! Input range: 22,1025~65534${CEND}"
+fi
+done
+ if [ -z "`grep ^Port /etc/ssh/sshd_config`" -a "$SSH_PORT" != '22' ];then
+sed -i "s@^#Port.*@&\nPort $SSH_PORT@" /etc/ssh/sshd_config
+elif [ -n "`grep ^Port /etc/ssh/sshd_config`" ];then
+sed -i "s@^Port.*@Port $SSH_PORT@" /etc/ssh/sshd_config
+fi
+service sshd restart
+fi
+fi
+
+echo "ssh port is already custom port $ssh_port"
+
+
+if ! [ -f /usr/sbin/csf ] ; then
+
+echo "2) Installing CSF"
+wget https://download.configserver.com/csf.tgz
+tar -xzf csf.tgz
+cd csf
+sh install.sh $*  >>  $LOG 2>&1
+phpret=$?
+# Was there an error
+if ! [ $phpret == "8" ]; then
+        echo " "
+        echo "ERROR :"
+        echo "There was an error while installing CSF"
+   echo "Please check /root/maintain.log for errors"
+        echo "Exiting Installer"
+        exit 1;
+fi
+
+else
+echo "csf is found"
+fi
+
+if [ -e "/etc/csf/csf.conf" ];then
+TEST=`grep ^TESTING /etc/csf/csf.conf | grep -v 'TESTING_INTERVAL' | awk '{print $3}' | cut -d'"'  -f2`
+echo $TEST
+if [ $TEST == "1" ]; then
+sed -i 's/TESTING = "1"/TESTING = "0"/' /etc/csf/csf.conf
+csf -r
+fi
+fi
+
+
+
+if ! [ -f /usr/local/sbin/maldet  ] ; then
+
+echo "2) Installing Maldet"
+wget http://www.rfxn.com/downloads/maldetect-current.tar.gz
+tar -xzvf maldetect-current.tar.gz
+cd maldetect-*
+sh install.sh $*  >>  $LOG 2>&1
+#phpret=$?
+# Was there an error
+#if ! [ $phpret == "8" ]; then
+ #       echo " "
+ #       echo "ERROR :"
+ #       echo "There was an error while installing Maldet"
+ #  echo "Please check /root/maintain.log for errors"
+ #       echo "Exiting Installer"
+ #       exit 1;
+#fi
+
+else
+echo "maldet is found"
+fi
+
+if ! [ -f /usr/local/cpanel/whostmgr/docroot/cgi/configserver/cse/cse.conf  ] ; then
+
+echo "2) Installing CSE "
+wget https://download.configserver.com/cse.tgz
+tar -xzf cse.tgz
+cd cse
+sh install.sh
+
+else
+echo "cse is found"
+fi
+
+if ! [ -f /etc/cmq/cmq.conf  ] ; then
+
+echo "2) Installing CMQ "
+wget http://download.configserver.com/cmq.tgz
+tar -xzf cmq.tgz
+cd cmq
+sh install.sh
+
+else
+echo "CMQ is found"
+fi
+
+
+if ! [ -f /usr/local/cpanel/whostmgr/docroot/cgi/configserver/cmm/cmm.conf  ] ; then
+
+echo "2) Installing CMM "
+wget http://download.configserver.com/cmm.tgz
+tar -xzf cmm.tgz
+cd cmm
+sh install.sh
+else
+echo "CMM is found"
+fi
+
+if ! [ -f /usr/local/cpanel/whostmgr/docroot/cgi/configserver/cmc/cmc.conf  ] ; then
+
+echo "2) Installing CMC "
+wget http://download.configserver.com/cmc.tgz
+tar -xzf cmc.tgz
+cd cmc
+sh install.sh
+else
+echo "CMC is found"
+fi
+
+
+while true; do
+    read -p "Do you wish to secure tmp partition?" yn
+    case $yn in
+        [Yy]* ) /scripts/securetmp; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
